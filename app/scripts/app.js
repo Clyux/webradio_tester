5a64971069cb18759f7e311a412b5b46b78ed766
Alfirin <grand.jimmy1@gmail.com>
2013-11-02 16:21:50 -0700

    Create view and template with modernizr

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('expressAngularApp', [])
+angular.module('septWebRadioApp', [])
   .config(['$routeProvider', function ($routeProvider) {
     $routeProvider
       .when('/', {
