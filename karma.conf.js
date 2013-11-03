34f0fb6ffb4792905b603cccde63263e7cd280cb
Alfirin <grand.jimmy1@gmail.com>
2013-11-02 17:50:08 -0700

    Correct import for testing

--- karma.conf.js
+++ karma.conf.js
@@ -8,8 +8,8 @@ files = [
   JASMINE,
   JASMINE_ADAPTER,
   'app/bower_components/angular/angular.js',
+  'app/bower_components/angular-route/angular-route.js',
   'app/bower_components/angular-mocks/angular-mocks.js',
-  'app/scripts/*.js',
   'app/scripts/**/*.js',
   'test/mock/**/*.js',
   'test/spec/**/*.js'
