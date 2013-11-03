0a277687e07767301074903be641bc43711ce9e7
Alfirin <grand.jimmy1@gmail.com>
2013-11-02 17:20:53 -0700

    Update angular version and create routing

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,40 @@
 'use strict';
 
-angular.module('septWebRadioApp', [])
-  .config(['$routeProvider', function ($routeProvider) {
+var septWebRadioApp = angular.module('septWebRadioApp', [
+  'ngRoute',
+  'septWebRadioControllers'
+]);
+
+septWebRadioApp.config(function($routeProvider, $locationProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/stage', {
+        templateUrl: 'views/stage/stage.html',
+        controller: 'StageCtrl'
+      })
+      .when('/replay', {
+        templateUrl: 'views/replay/replay.html',
+        controller: 'ReplayCtrl'
+      })
+      .when('/topical', {
+        templateUrl: 'views/topical/topical.html',
+        controller: 'TopicalCtrl'
+      })
+      .when('/door', {
+        templateUrl: 'views/door/door.html',
+        controller: 'DoorCtrl'
+      })
+      .when('/backstage', {
+        templateUrl: 'views/backstage/backstage.html',
+        controller: 'BackstageCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
-  }]);
+
+    // configure html5 to get links working on jsfiddle
+    $locationProvider.html5Mode(true);
+});
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,40 @@
 'use strict';
 
-angular.module('septWebRadioApp')
-  .controller('MainCtrl', ['$scope', function ($scope) {
+/* Controllers */
+
+var appControllers = angular.module('septWebRadioControllers', []);
+
+appControllers.controller('MainCtrl', ['$scope',
+  function($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  }]);
+  }]
+);
+
+appControllers.controller('StageCtrl', ['$scope',
+  function($scope) {
+  }]
+);
+
+appControllers.controller('ReplayCtrl', ['$scope',
+  function($scope) {
+  }]
+);
+
+appControllers.controller('TopicalCtrl', ['$scope',
+  function($scope) {
+  }]
+);
+
+appControllers.controller('DoorCtrl', ['$scope',
+  function($scope) {
+  }]
+);
+
+appControllers.controller('BackstageCtrl', ['$scope',
+  function($scope) {
+  }]
+);
