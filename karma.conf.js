da5dff54bb45acda506d1c4bbbe2a338cb2f0a59
Alfirin <grand.jimmy1@gmail.com>
2013-11-08 16:05:40 -0800

    Update the test command for travis

--- karma.conf.js
+++ karma.conf.js
@@ -19,7 +19,7 @@ module.exports = function(config) {
       'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
       //'node_modules/socket.io/node_modules/socket.io-client/dist/socket.io.js',
-      'app/scripts/**/*.js',
+      'dist/scripts/**/*.js',
       'test/mock/**/*.js',
       'test/unit/**/*.js'
     ],
