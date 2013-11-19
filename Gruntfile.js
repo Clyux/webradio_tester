d01de8c5021ee4a95cb4b0b0ed79fc5afbde4ed4
Alfirin <grand.jimmy1@gmail.com>
2013-11-19 05:51:13 -0800

    Create masonry layout

--- Gruntfile.js
+++ Gruntfile.js
@@ -100,7 +100,8 @@ module.exports = function (grunt) {
     },
     jshint: {
       options: {
-        jshintrc: '.jshintrc'
+        jshintrc: '.jshintrc',
+        ignores: ['<%= yeoman.app %>/scripts/libs/{,*/}*.js']
       },
       all: [
         'Gruntfile.js',
