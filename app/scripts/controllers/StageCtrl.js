4bd798c5827cd8d40a84036e3aa213f17f9ea37e
Alfirin <grand.jimmy1@gmail.com>
2013-11-22 11:56:13 -0800

    Create a smooth animation

--- app/scripts/controllers/StageCtrl.js
+++ app/scripts/controllers/StageCtrl.js
@@ -16,19 +16,17 @@ angular.module('septWebRadioControllers')
         if ($scope.searchedTerm && $scope.searchedTerm !== '' && $scope.searchedTerm.length >= 2) {
           $scope.isSearching = true;
 
-          // Remove all the items for the list.
-          $scope.searchedItems.splice(0, $scope.searchedItems.length);
-
           // Search the terms
           soundcloudSearch.autoCompleteSearch($scope.searchedTerm)
             .then(function (response) {
-              $scope.searchedItems = limitToFilter(response, 20);
+              var tracks = limitToFilter(response, 20);
+              $scope.searchedItems = tracks;
               $scope.isSearching = false;
             });
         } else {
           // There is nothing to search
           $scope.isSearching = false;
-          $scope.searchedItems = [];
+          $scope.searchedItems.splice(0, $scope.searchedItems.length);
         }
       };
     }]
